b0ebf8938ee03e9da9c43b7f5fbaf6d662e6f25d
Caio Costa <caiocostaviana2016@gmail.com>
2022-05-14 18:31:12 -0400

    Aprendendo a declarar objetos e suas propriedades

---
+++ exercicio2.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome : prompt('Digite seu nome: '),
+    sobrenome : prompt('Digite seu sobrenome'),
+    idade : prompt('Digite sua idade: '),
+    corFavorita : prompt('Qual sua cor favorita? ')
+}
+console.log(pessoa)
+//neste exercício ,aprendi a declarar um objeto , e suas propriedades.

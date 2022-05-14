69c9e7360fcea3a75896797f0c1ed46cbb4ce2a2
Caio Costa <caiocostaviana2016@gmail.com>
2022-05-13 21:34:41 -0400

    Iniciando os estudos em JavaScript

---
+++ scrypt.js
@@ -0,0 +1,7 @@
+let nota1 = prompt('Digite a primeira nota: ')
+let nota2 = prompt('Digite a segunda nota: ')
+if(nota1 < nota2){
+    console.log('A segunda nota é maior que a primeira.')
+}else{
+    console.log('A primeira nota é maior que a segunda')
+}
